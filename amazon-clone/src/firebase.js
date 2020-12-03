e608dc988ca238c15d331f424895bd81e8a1ce28
Zach S.L <zach.s.ledford@gmail.com>
2020-12-02 19:26:58 -0500

    styled header for app

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVh3dMTPCMwPVCsm5WhlupXvv-gbgKcLY",
+    authDomain: "clone-zl-46309.firebaseapp.com",
+    databaseURL: "https://clone-zl-46309.firebaseio.com",
+    projectId: "clone-zl-46309",
+    storageBucket: "clone-zl-46309.appspot.com",
+    messagingSenderId: "230884447945",
+    appId: "1:230884447945:web:004e5c16e95069da71df18",
+    measurementId: "G-JFME0JS86C"
+  };
