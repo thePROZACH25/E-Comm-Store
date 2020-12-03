54d154b85867a5d0fa4ac2f33da3bf56e756f82f
Zach S.L <zach.s.ledford@gmail.com>
2020-12-02 23:05:14 -0500

    styled home page and adding routes in app.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import Header from '../src/components/Header'
+import "./App.css";
+import Header from "../src/components/Header";
+import Home from "../src/components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header />
-      {/* Home */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <h1>I am a checkout page!!!</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
