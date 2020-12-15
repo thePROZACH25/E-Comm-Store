510376274bf78ba086f175c6e1c623169dc862e2
Zach S.L <zach.s.ledford@gmail.com>
2020-12-15 14:03:15 -0500

    displaying user email in basket and in header

--- amazon-clone/src/components/Checkout.js
+++ amazon-clone/src/components/Checkout.js
@@ -6,7 +6,7 @@ import CheckoutProduct from '../components/CheckoutProduct'
 
 function Checkout() {
 
-  const [ { basket }, dispatch] = useStateValue();
+  const [ { basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -17,6 +17,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
 
             {basket.map(item =>
