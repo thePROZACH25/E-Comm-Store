e12f14bcf86930e0dfc5ba9eb5d09033596fb3dc
Zach S.L <zach.s.ledford@gmail.com>
2021-01-07 11:59:03 -0500

    pushed to firebase for hosting

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
     // THE API (cloud function) URL
-    baseURL: 'http://localhost:5001/clone-zl-46309/us-central1/api', 
+    baseURL: 'https://us-central1-clone-zl-46309.cloudfunctions.net/api'
+    // Local Host link
+    // 'http://localhost:5001/clone-zl-46309/us-central1/api' 
+    
 });
 
 export default instance;
